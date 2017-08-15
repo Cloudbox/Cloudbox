721e5cc270be20848e4ad2d9ccc1d57993470d39
l3uddz <l3uddz@gmail.com>
2017-08-15 05:02:40 +0100

    implement custom smoothstreams container, with credits to notorious's ondemand auth refresher/dynamic playlists scripts. hopefully this integrates nicely with tvheadend and live channels.

---
+++ roles/iptv/files/sstv.php
@@ -0,0 +1,35 @@
+<?php
+// EPG - http://sstv.fog.pt/feed.xml
+
+if (isset($_GET['ch'])) {
+    $config = parse_ini_file('config.ini');
+
+    $auth = file_exists('./cache/auth.ini') ? parse_ini_file('./cache/auth.ini') : null;
+
+    if (is_null($auth) || $auth['time'] < time()) {
+        $hash = json_decode(file_get_contents('https://auth.smoothstreams.tv/hash_api.php?username='
+            . urlencode($config['username']) . '&password=' . urlencode($config['password']) . '&site='
+            . urlencode($config['service'])), true)['hash'];
+
+        file_put_contents('./cache/auth.ini', '[auth]' . PHP_EOL
+            . 'hash = "' . $hash . '"' . PHP_EOL . 'time = ' . (time() + 12600) . PHP_EOL);
+
+        $auth = parse_ini_file('./cache/auth.ini');
+    }
+
+    $url = 'http://' . $config['server'] . '.smoothstreams.tv:9100/' . $config['service']
+        . '/ch' . ($_GET['ch'] < 10 ? '0' : '') . $_GET['ch'] . 'q1.stream/';
+
+    file_put_contents('./cache/playlist.m3u8', str_replace('chunks', $url . 'chunks',
+        file_get_contents($url . 'playlist.m3u8?wmsAuthSign=' . $auth['hash'] . '==')));
+
+    header('Content-Disposition: attachment; filename="playlist.m3u8"');
+    header('Content-Length: ' . filesize('./cache/playlist.m3u8'));
+    header('Content-Type: ' . mime_content_type('./cache/playlist.m3u8'));
+
+    readfile('./cache/playlist.m3u8');
+
+    exit();
+}
+
+phpinfo();
