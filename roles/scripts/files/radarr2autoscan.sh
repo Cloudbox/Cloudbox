7f62057ac0ef84118285a4cda50a2a8b11777a27
desimaniac <desimaniac@msn.com>
2017-10-30 12:47:25 -0500

    Redo of the previous script
    will need to be filled in manually.

--- roles/scripts/files/radarr2autoscan.sh
+++ roles/scripts/files/radarr2autoscan.sh
@@ -2,20 +2,7 @@
 # curl autoscan radarr webhook sender
 
 # settings
-
-cfg_server_ip=$(cat /opt/plex_autoscan/config/config.json | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["'SERVER_IP'"]';)
-
-if [ $cfg_server_ip = 0.0.0.0 ]; then
-  server_ip="$(curl -s http://checkip.amazonaws.com)"
-else
-  server_ip=$cfg_server_ip
-fi
-
-server_port=$(cat /opt/plex_autoscan/config/config.json | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["'SERVER_PORT'"]';)
-
-server_pass=$(cat /opt/plex_autoscan/config/config.json | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["'SERVER_PASS'"]';)
-
-AUTOSCAN_URL=http://${server_ip}:${server_port}/${server_pass}
+AUTOSCAN_URL=
 
 # args/envs
 MOVIE_PATH="$radarr_moviefile_path"
