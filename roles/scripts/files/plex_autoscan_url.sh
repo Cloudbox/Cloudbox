72c53d3abd33a06cb73b112e15e535aa8e7267ba
desimaniac <desimaniac@users.noreply.github.com>
2018-04-16 02:16:05 -0500

    Scripts: Plex Autoscan URL script now shows domain URL no matter what.

--- roles/scripts/files/plex_autoscan_url.sh
+++ roles/scripts/files/plex_autoscan_url.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-## Script prints the Plex Autoscan URL
+## Script prints the Plex Autoscan URL.
+## Author: desimaniac
 
 
-# Get variable(s) from Plex Autoscan config
+# Get variable(s) from Plex Autoscan config.
 server_ip=$(cat /opt/plex_autoscan/config/config.json | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["'SERVER_IP'"]';)
 server_port=$(cat /opt/plex_autoscan/config/config.json | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["'SERVER_PORT'"]';)
 server_pass=$(cat /opt/plex_autoscan/config/config.json | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["'SERVER_PASS'"]';)
 
-# Get variable(s) from Ansible settings
+# Get variable(s) from Ansible settings.
 domain="plex.$(cat $HOME/cloudbox/settings.yml | python3 -c 'import ruamel.yaml,sys;obj=ruamel.yaml.round_trip_load(sys.stdin);print (obj["'domain'"])';)"
+
+# Print Plex Autoscan URL with domain.
+echo "Plex Autoscan URL:"
+echo "=================="
+echo "http://${domain}:${server_port}/${server_pass}"
 
 # If 0.0.0.0, assign real IP address.
 if [ $server_ip = 0.0.0.0 ]; then
@@ -21,12 +27,7 @@ fi
 # Get IP address of domain (will work with most TLDs)
 domain_ip=$(python -c "import socket; print(socket.gethostbyname(\"$domain\"))")
 
-# If domain's IP matches real IP, use domain in URL.
-if [ "$real_ip" = "$domain_ip" ]; then
-  server_host=$domain
-else
-  server_host=$real_ip
+# If domain's IP does not match real IP, print Plex Autoscan URL with IP Address.
+if [ "$real_ip" != "$domain_ip" ]; then
+  echo "http://${real_ip}:${server_port}/${server_pass} (use this if the one above does not work)"
 fi
-
-# Print Plex Autoscan URL
-echo "http://${server_host}:${server_port}/${server_pass}"
