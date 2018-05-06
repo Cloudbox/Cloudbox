61b2cbd65040606a0c3a4ece64056ee8f9f4e285
desimaniac <desimaniac@users.noreply.github.com>
2018-05-06 17:07:41 -0500

    Plex Autoscan: Fixed up Plex Autoscan URL Script
    - will now check subdomains against IP (ie domains setup behind CDN wont show up).
    - added headers to various other scripts.
    - added jq, yq, and dnsutils to common role (left jq in motd role for now).

--- roles/scripts/files/restart_containers.sh
+++ roles/scripts/files/restart_containers.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-# Stop running containers and start them back up.
+#########################################################################
+# Title:         Restart Containers                                     #
+# Author:        desimaniac                                             #
+# URL:           https://github.com/Cloudbox/Cloudbox                   #
+# Description:   Stop running containers and start them back up.        #
+# --                                                                    #
+# Part of the Cloudbox project: https://cloudbox.rocks                  #
+#########################################################################
+# GNU General Public License v3.0                                       #
+#########################################################################
 
 containers=$(comm -12 <(docker ps -a -q | sort) <(docker ps -q | sort))
 for container in $containers;
