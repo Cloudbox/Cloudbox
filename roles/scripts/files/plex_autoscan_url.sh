d1fee0065f15203c85fa18632197e6fdaf1f5d3b
desimaniac <desimaniac@users.noreply.github.com>
2018-05-06 17:07:41 -0500

    Plex Autoscan: Fixed up Plex Autoscan URL Script
    - will now check subdomains against IP (ie domains setup behind CDN wont show up).
    - added headers to various other scripts.
    - added jq, yq, and dnsutils to common role (left jq in motd role for now).

--- roles/scripts/files/plex_autoscan_url.sh
+++ roles/scripts/files/plex_autoscan_url.sh
@@ -1,21 +1,44 @@
 #!/bin/bash
+#########################################################################
+# Title:         Plex Autoscan URL                                      #
+# Author:        desimaniac                                             #
+# URL:           https://github.com/Cloudbox/Cloudbox                   #
+# Description:   Script displays the Plex Autoscan URL                  #
+# --                                                                    #
+# Part of the Cloudbox project: https://cloudbox.rocks                  #
+#########################################################################
+# GNU General Public License v3.0                                       #
+#########################################################################
 
-## Script prints the Plex Autoscan URL.
-## Author: desimaniac
+cat <<BANNER
+
+ ____  _                _         _                              _   _ ____  _
+|  _ \| | _____  __    / \  _   _| |_ ___  ___  ___ __ _ _ __   | | | |  _ \| |
+| |_) | |/ _ \ \/ /   / _ \| | | | __/ _ \/ __|/ __/ _\` | '_ \  | | | | |_) | |
+|  __/| |  __/>  <   / ___ \ |_| | || (_) \__ \ (_| (_| | | | | | |_| |  _ <| |___
+|_|   |_|\___/_/\_\ /_/   \_\__,_|\__\___/|___/\___\__,_|_| |_|  \___/|_| \_\_____|
 
 
-# Get variable(s) from Plex Autoscan config.
-server_ip=$(cat /opt/plex_autoscan/config/config.json | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["'SERVER_IP'"]';)
-server_port=$(cat /opt/plex_autoscan/config/config.json | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["'SERVER_PORT'"]';)
-server_pass=$(cat /opt/plex_autoscan/config/config.json | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["'SERVER_PASS'"]';)
+###################################################################################
+# Title:         Plex Autoscan URL                                                #
+# Author:        desimaniac                                                       #
+# URL:           https://github.com/Cloudbox/Cloudbox                             #
+# Description:   Prints out the the Plex Autoscan URL.                            #
+# --                                                                              #
+# Part of the Cloudbox project: https://cloudbox.rocks                            #
+###################################################################################
+# GNU General Public License v3.0                                                 #
+###################################################################################
 
-# Get variable(s) from Ansible settings.
-domain="plex.$(cat $HOME/cloudbox/settings.yml | python3 -c 'import ruamel.yaml,sys;obj=ruamel.yaml.round_trip_load(sys.stdin);print (obj["'domain'"])';)"
+BANNER
 
-# Print Plex Autoscan URL with domain.
-echo "Plex Autoscan URL:"
-echo "=================="
-echo "http://${domain}:${server_port}/${server_pass}"
+# Get variable(s) from Plex Autoscan config
+server_ip=$(cat /opt/plex_autoscan/config/config.json | jq -r .SERVER_IP)
+server_port=$(cat /opt/plex_autoscan/config/config.json | jq -r .SERVER_PORT)
+server_pass=$(cat /opt/plex_autoscan/config/config.json | jq -r .SERVER_PASS)
+
+# Get variable(s) from Ansible settings
+domain=$(cat $HOME/cloudbox/settings.yml | yq -r .domain)
 
 # If 0.0.0.0, assign real IP address.
 if [ $server_ip = 0.0.0.0 ]; then
@@ -24,10 +47,30 @@ else
   real_ip=$server_ip
 fi
 
-# Get IP address of domain (will work with most TLDs)
-domain_ip=$(python -c "import socket; print(socket.gethostbyname(\"$domain\"))")
 
-# If domain's IP does not match real IP, print Plex Autoscan URL with IP Address.
-if [ "$real_ip" != "$domain_ip" ]; then
-  echo "http://${real_ip}:${server_port}/${server_pass} (use this if the one above does not work)"
-fi
+# Declare Subdomains Array
+declare -a subdomains=(
+  "plex.$domain"
+  "plexbox.$domain"
+  "cloudbox.$domain"
+  "$real_ip"
+)
+
+# Get length of the subdomains array
+subdomain_len=${#subdomains[@]}
+
+# Declare variables for while loop
+declare -i count=0
+subdomain_ip=""
+
+# Get subdomain who's IP address matches real_ip
+while [[ (($real_ip != $subdomain_ip) && ($count < ${subdomain_len})) ]]; do
+    subdomain=${subdomains[$count]}
+    subdomain_ip=$(dig +short ${subdomain})
+    count+=1
+done
+
+# Print Plex Autoscan URL
+echo "Your Plex Autoscan URL is:"
+echo "http://${subdomain}:${server_port}/${server_pass}"
+echo ""
