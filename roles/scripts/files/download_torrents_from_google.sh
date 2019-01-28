d4b961d3113e3d0e2c04545a93c64a6939cedf11
desimaniac <5501908+desimaniac@users.noreply.github.com>
2019-01-28 01:20:58 -0600

    Scripts: Update torrent syncing scripts to match new rclone remote variable.
    - Oher misc tweaks.

--- roles/scripts/files/download_torrents_from_google.sh
+++
@@ -1,4 +0,0 @@
-if ! /usr/bin/screen -list | /bin/grep -q "torrents_download"; then
-	/bin/rm -rfv /opt/scripts/rclone/download_to_disk.log
-	/usr/bin/screen -dmS torrents_download /usr/bin/rclone copy google:/downloads/torrents /mnt/local/downloads/torrents --config=/home/seed/.config/rclone/rclone.conf --verbose=1 --transfers=8 --stats=60s --checkers=16 --drive-chunk-size=128M --fast-list --log-file=/opt/scripts/rclone/download_to_disk.log
-fi
