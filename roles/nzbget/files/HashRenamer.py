6965504db7cff8804304a83abff8eafd2d56b494
desimaniac <5501908+desimaniac@users.noreply.github.com>
2019-03-25 21:58:51 -0500

    NZBGet: Attribution update to HashRenamer.py script.
    Attribution was given to 
    [Clinton-Hall](https://github.com/clinton-hall/GetScripts) since the 
    base template of their NZBGet scripts was used. But despite what 
    copy-cat groups think, 90-99 percent of the script was built by us. But 
    unlike them, we dont copy entire scripts from others and rename it to 
    obfuscate from the origin source - that's their modus operandi, not 
    ours. ;)

--- roles/nzbget/files/HashRenamer.py
+++ roles/nzbget/files/HashRenamer.py
@@ -8,7 +8,11 @@
 # --                                                                         #
 #            Part of the Cloudbox project: https://cloudbox.works            #
 ##############################################################################
-#                     GNU General Public License v3.0                        #
+
+##############################################################################
+#  Built on top of the NZBGet scripts template created by Clinton Hall       #
+#    (https://github.com/clinton-hall).                                      #
+#  Released under GNU General Public License v2.0                            #
 ##############################################################################
 
 
