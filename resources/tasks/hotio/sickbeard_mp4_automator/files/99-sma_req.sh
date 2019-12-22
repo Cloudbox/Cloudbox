9397a927bc55714cdc8221589f26748c62b03f49
desimaniac <5501908+desimaniac@users.noreply.github.com>
2019-12-22 15:02:06 -0600

    Resources: Hotio images SMA requirements installer task

---
+++ resources/tasks/hotio/sickbeard_mp4_automator/files/99-sma_req.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/with-contenv bash
+
+#########################################################################
+# Title:         Cloudbox: SMA Requirements Installer Script            #
+# Author(s):     desimaniac                                             #
+# URL:           https://github.com/cloudbox/cloudbox                   #
+# --                                                                    #
+#         Part of the Cloudbox project: https://cloudbox.works          #
+#########################################################################
+#                   GNU General Public License v3.0                     #
+#########################################################################
+
+if [[ ! -f /sma_req_installed ]]; then
+    apt update
+    apt install -y --no-install-recommends --no-install-suggests \
+        python-pip \
+        python-setuptools \
+        ffmpeg
+    pip --no-cache-dir install \
+        requests \
+        requests[security] \
+        requests-cache \
+        babelfish \
+        stevedore==1.19.1 \
+        python-dateutil \
+        deluge-client \
+        qtfaststart \
+        "guessit<2" \
+        "subliminal<2"
+    touch /sma_req_installed
+fi
