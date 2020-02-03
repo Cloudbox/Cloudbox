9c7d7984064841329793753d6dc098544f8d0c6e
desimaniac <5501908+desimaniac@users.noreply.github.com>
2020-02-02 23:30:10 -0600

    Python-PlexLibrary: Updated helper scripts
    - Added a new helper script separate from the cron helper one

--- roles/python-plexlibrary/files/plexlibrary-cron-helper.sh
+++ roles/python-plexlibrary/files/plexlibrary-cron-helper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #########################################################################
-# Title:         Cloudbox: Python-PlexLibrary Helper Script             #
+# Title:         Cloudbox: Python-PlexLibrary Cron Helper Script        #
 # Author(s):     desimaniac                                             #
 # URL:           https://github.com/cloudbox/cloudbox                   #
 # --                                                                    #
@@ -13,11 +13,17 @@ PATH='/usr/bin:/bin:/usr/local/bin'
 export PYTHONIOENCODING=UTF-8
 
 PYTHON_PATH='/usr/bin/python'
-LOG_PATH='/opt/python-plexlibrary/plexlibrary.log'
+LOG_PATH='/opt/python-plexlibrary/plexlibrary-cron.log'
 RECIPES_PATH='/opt/python-plexlibrary/recipes'
 SCRIPT_PATH='/opt/python-plexlibrary/plexlibrary/plexlibrary.py'
 
 echo $(date) | tee -a $LOG_PATH
 echo "" | tee -a $LOG_PATH
 
-$PYTHON_PATH $SCRIPT_PATH $RECIPES_PATH/"$@"
+for file in $RECIPES_PATH/*
+do
+    if [ -f "${file}" ]; then
+        $PYTHON_PATH $SCRIPT_PATH $(basename "$file" .yml) | tee -a $LOG_PATH
+        echo "" | tee -a $LOG_PATH
+    fi
+done
